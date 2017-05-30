569980155783445b5f8e92f36257af270dd62a8c
DinaEMusalam <dina.musalam@softhouse.se>
2017-05-30 11:54:17 +0200

    Update apiService.js

--- js/services/apiService.js
+++ js/services/apiService.js
@@ -1,6 +1,7 @@
 ArtifactoryApp.service('APIService', function() {
   
     var APIUrl = "http://localhost:3000/api/";
+  // this url must change to the server after deploy 
 
     var repositories = APIUrl + "repositories/";
     var storage = APIUrl + "storage/";
